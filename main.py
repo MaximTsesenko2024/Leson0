6007d1bf186cdf3132729729c1cdc433f86916d7
MaximTsesenko2024 <maksts1991@yandex.ru>
2024-05-30 21:07:36 +0300

    Lesen 0

---
+++ main.py
@@ -0,0 +1,21 @@
+a='Test string'
+print(len(a))
+first=10
+second=3
+summa=first+second
+diff=first-second
+print(summa,diff)
+first=45
+second=23
+third=8
+mean=(first+second+third)/3
+print(mean)
+first_string='Вторник'
+second_string='Понедельник'
+print(second_string+', '+first_string)
+a=34
+b=12
+c=54
+f=a*b+a*c
+d=f**3/2
+print(d)
